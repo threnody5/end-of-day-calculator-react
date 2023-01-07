9d7e5757d16ab34623d17053c6b6390c91dd7c93
William <williamw15@protonmail.com>
2023-01-07 12:49:31 -0500

    add date functionality to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,48 @@
 /** @format */
 
+// import Date from '../Date/Date';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './Header.css';
+
 export default function Header() {
+
+  const monthName = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  let day = date.getDate();
+
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  const currentMonth = monthName[month];
+  
   return (
-    <div>
-      <h1>Header Works!</h1>
+    <div className='header-container pt-5'>
+      <div>
+        <h1 className='header-title'>EOD Calculator</h1>
+      </div>
+      <div>
+        {/* <Date /> */}
+        {currentMonth}, {day}, {year}
+      </div>
     </div>
   );
 }
