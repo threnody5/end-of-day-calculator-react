6e82811a24620205d2d1d1e6caa6cab7017add7b
William <williamw15@protonmail.com>
2023-01-13 15:20:29 -0500

    changes made to create error: too much recursion

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,42 +1,42 @@
 /** @format */
 
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
 export default function Date() {
-//   const monthName = [
-//     'Jan',
-//     'Feb',
-//     'Mar',
-//     'Apr',
-//     'May',
-//     'Jun',
-//     'Jul',
-//     'Aug',
-//     'Sep',
-//     'Oct',
-//     'Nov',
-//     'Dec',
-//   ];
+  const monthName = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
 
-//   const date = new Date();
+  const date = new Date();
 
-//   const year = date.getFullYear();
-//   const month = date.getMonth();
-//   let day = date.getDate();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  let day = date.getDate();
 
-//   if (day < 10) {
-//     day = '0' + day;
-//   }
+  if (day < 10) {
+    day = '0' + day;
+  }
 
-//   const currentMonth = monthName[month];
+  const currentMonth = monthName[month];
 
-//   return (
-//     <div>
-//       <div>
-//         <h3>
-//           {currentMonth}, {day}, {year}
-//         </h3>
-//       </div>
-//     </div>
-//   );
+  return (
+    <div>
+      <div>
+        <h3>
+          {currentMonth}, {day}, {year}
+        </h3>
+      </div>
+    </div>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,38 @@
 /** @format */
 
-// import Date from '../Date/Date';
+import Date from '../Date/Date';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './Header.css';
 
 export default function Header() {
 
-  const monthName = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
+  // const monthName = [
+  //   'Jan',
+  //   'Feb',
+  //   'Mar',
+  //   'Apr',
+  //   'May',
+  //   'Jun',
+  //   'Jul',
+  //   'Aug',
+  //   'Sep',
+  //   'Oct',
+  //   'Nov',
+  //   'Dec',
+  // ];
 
-  const date = new Date();
+  // const date = new Date();
 
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  let day = date.getDate();
+  // const year = date.getFullYear();
+  // const month = date.getMonth();
+  // let day = date.getDate();
 
-  if (day < 10) {
-    day = '0' + day;
-  }
+  // if (day < 10) {
+  //   day = '0' + day;
+  // }
 
-  const currentMonth = monthName[month];
+  // const currentMonth = monthName[month];
   
   return (
     <div className='header-container pt-3'>
@@ -40,8 +40,8 @@ export default function Header() {
         <h1 className='header-title'><strong>EOD Calculator</strong></h1>
       </div>
       <div>
-        {/* <Date /> */}
-        <h3 className='date'>{currentMonth}, {day}, {year}</h3>
+        <Date />
+        {/* <h3 className='date'>{currentMonth}, {day}, {year}</h3> */}
       </div>
     </div>
   );
